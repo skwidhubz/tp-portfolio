d5ca6a6d4b9f100e0723c9d4e79c228c33decfdc
skwidhubz <tommaxwellparker@gmail.com>
2023-06-10 14:48:34 +1000

    nav and routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Nav from './Components/Nav';
+import Projects from './Components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav />
+      <div className="main-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ portfolio/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+        <div>
+        <p>
+            This is the home page
+        </p>
+        </div>
+        </>
+    )
+}
+
+export default Home;
