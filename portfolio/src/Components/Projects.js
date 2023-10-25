e1b9e447e8a480cd4b71a011d25fdc27b03e4a5f
skwidhubz <tommaxwellparker@gmail.com>
2023-10-25 21:51:16 +1100

    brekas

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -8,7 +8,7 @@ import '../Styles/projects.css';
 function Projects() {
   return (
     <div className='projects-container'> 
-      <span className="projects-header">PROJECTS</span>
+      {/* <span className="projects-header">PROJECTS</span> */}
       <Carousel>
       <Carousel.Item>
           <img
